ec50f3383c282741897a32f24e18a322ec426fa5
124110496 JULIO KANTARELLY DOS SANTOS LIMA <julio.kantarelly.dos.santos.lima@lcc1-17.lcc.ufcg.edu.br>
2025-07-04 13:41:40 -0300

    test

---
+++ src/main/java/sorting/variationsOfInsertionsort/RecursiveInsertionSort.java
@@ -0,0 +1,51 @@
+package sorting.variationsOfInsertionsort;
+
+import sorting.AbstractSorting;
+
+public class RecursiveInsertionSort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+
+	/**
+	 * Implementacaoo RECURSIVA do insertion sort. Para isso, tente definir o 
+	 * caso base do algoritmo e depois o caso recursivo, que reduz o problema 
+	 * para uma entrada menor em uma chamada recursiva. Seu algoritmo deve 
+	 * ter complexidade quadratica O(n^2).
+	 * 
+	 * Restrições:
+	 *  - voce NAO pode utilizar array auxiliar
+	 *  - voce pode utilizar variaveis temporarias
+	 *  - voce NAO pode declarar novos atributos na classe
+	 *  - para as trocas no array, utilize o metodo Util.swap
+	 */
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		
+
+		if (leftIndex < 0 || leftIndex > rightIndex || rightIndex > array.length || leftIndex > array.length || rightIndex < 0) {
+
+		} 
+
+		if (leftIndex == rightIndex) {
+
+		} else {
+			T key = array[leftIndex + 1];
+			int i = leftIndex;
+			while (i >= 0 && (key.compareTo(array[i]) < 0)) { 
+				util.Util.swap(array, i + 1, i);
+				i--;
+			}
+
+			sort(array, leftIndex + 1, rightIndex);
+		}
+	}
+}
+
+	// for (int j = leftIndex + 1; j < rightIndex; j++) {
+		// 	T key = array[j];
+		// 	int i = j - 1;
+		// 	while (i >= 0 && (key.compareTo(array[i]) < 0)) {
+		// 		array[i + 1] = array[i];
+		// 		i--;
+		// 	}
+		// 	array[i + 1] = key;
+		// }
